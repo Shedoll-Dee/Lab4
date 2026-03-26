df2076bc09135d2f6b2883054e7ae9c2ceca106e
Dee <dianakud20060127@gmail.com>
2026-03-26 21:37:12 +0500

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import OrdinalEncoder
 
-def download_data():
-    df = pd.read_csv('./adult.csv', delimiter = ',')
-    df.to_csv("cars.csv", index = False)
-    return df
 
 def clear_data(path2df):
-    df = pd.read_csv(path2df)
+    df = pd.read_csv('./adult.csv', delimiter = ',')
     
     print(df.columns)
 
