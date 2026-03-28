58422e068cec37713e9df25d599c9cbdc9e0df2b
Dee <dianakud20060127@gmail.com>
2026-03-28 13:24:38 +0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,79 +1,100 @@
-
-from os import name
-from sklearn.preprocessing import StandardScaler, PowerTransformer
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import mlflow
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from mlflow.models import infer_signature
 import joblib
 
 
 def scale_frame(frame):
     df = frame.copy()
-    X,y = df.drop(columns = ['Price(euro)']), df['Price(euro)']
+
+    X = df.drop(columns=['income'])
+    y = df['income']
+
     scaler = StandardScaler()
-    power_trans = PowerTransformer()
     X_scale = scaler.fit_transform(X.values)
-    Y_scale = power_trans.fit_transform(y.values.reshape(-1,1))
-    return X_scale, Y_scale, power_trans
+
+    return X_scale, y.values
+
 
 def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+    accuracy = accuracy_score(actual, pred)
+    f1 = f1_score(actual, pred)
+    roc = roc_auc_score(actual, pred)
+
+    return accuracy, f1, roc
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("./df_clear.csv")
-    X,Y, power_trans = scale_frame(df)
-    X_train, X_val, y_train, y_val = train_test_split(X, Y,
-                                                    test_size=0.3,
-                                                    random_state=42)
-    
 
-    params = {'alpha': [0.0001, 0.001, 0.01, 0.05, 0.1 ],
-            'l1_ratio': [0.001, 0.05, 0.01, 0.2],
-            "penalty": ["l1","l2","elasticnet"],
-            "loss": ['squared_error', 'huber', 'epsilon_insensitive'],
-            "fit_intercept": [False, True],
-            }
-    
-    mlflow.set_experiment("linear model cars")
+    df = pd.read_csv("./df_clear.csv")
+
+    X, Y = scale_frame(df)
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, Y,
+        test_size=0.3,
+        random_state=42
+    )
+
+    params = {
+        'alpha': [0.0001, 0.001, 0.01],
+        'l1_ratio': [0.05, 0.1, 0.2],
+        'penalty': ["l1", "l2", "elasticnet"],
+        'loss': ['log_loss', 'hinge', 'modified_huber'],
+        'fit_intercept': [False, True],
+    }
+
+    mlflow.set_experiment("adult income classification")
+
     with mlflow.start_run():
-        lr = SGDRegressor(random_state=42)
-        clf = GridSearchCV(lr, params, cv = 3, n_jobs = 4)
-        clf.fit(X_train, y_train.reshape(-1))
+
+        clf_base = SGDClassifier(random_state=42)
+
+        clf = GridSearchCV(
+            clf_base,
+            params,
+            cv=3,
+            n_jobs=4
+        )
+
+        clf.fit(X_train, y_train)
+
         best = clf.best_estimator_
+
         y_pred = best.predict(X_val)
-        y_price_pred = power_trans.inverse_transform(y_pred.reshape(-1,1))
-        (rmse, mae, r2)  = eval_metrics(power_trans.inverse_transform(y_val), y_price_pred)
-        alpha = best.alpha
-        l1_ratio = best.l1_ratio
-        penalty = best.penalty
-        eta0 = best.eta0
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_param("penalty", penalty)
-        mlflow.log_param("eta0", eta0)
+
+        accuracy, f1, roc = eval_metrics(y_val, y_pred)
+
+        mlflow.log_param("alpha", best.alpha)
+        mlflow.log_param("l1_ratio", best.l1_ratio)
+        mlflow.log_param("penalty", best.penalty)
         mlflow.log_param("loss", best.loss)
         mlflow.log_param("fit_intercept", best.fit_intercept)
-        mlflow.log_param("epsilon", best.epsilon)
-        
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
-        
+
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("f1_score", f1)
+        mlflow.log_metric("roc_auc", roc)
+
         predictions = best.predict(X_train)
         signature = infer_signature(X_train, predictions)
+
         mlflow.sklearn.log_model(best, "model", signature=signature)
-        with open("lr_cars.pkl", "wb") as file:
-            joblib.dump(lr, file)
+
+        with open("sgd_income.pkl", "wb") as file:
+            joblib.dump(best, file)
 
     dfruns = mlflow.search_runs()
-    path2model = dfruns.sort_values("metrics.r2", ascending=False).iloc[0]['artifact_uri'].replace("file://","") + '/model' #путь до эксперимента с лучшей моделью
+
+    path2model = (
+        dfruns.sort_values("metrics.accuracy", ascending=False)
+        .iloc[0]['artifact_uri']
+        .replace("file://", "") + '/model'
+    )
+
     print(path2model)
