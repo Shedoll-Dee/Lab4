09260bf8de7dfac07a2b221cfcb70c5ba3895550
Dee <dianakud20060127@gmail.com>
2026-03-28 15:24:06 +0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ import mlflow
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 import numpy as np
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from mlflow.models import infer_signature
 import joblib
 
@@ -19,45 +19,48 @@ def scale_frame(frame):
     scaler = StandardScaler()
     X_scale = scaler.fit_transform(X.values)
 
-    return X_scale, y.values
+    return X_scale, y.values, scaler
 
 
 def eval_metrics(actual, pred):
+
     accuracy = accuracy_score(actual, pred)
     f1 = f1_score(actual, pred)
-    roc = roc_auc_score(actual, pred)
+    precision = precision_score(actual, pred)
+    recall = recall_score(actual, pred)
 
-    return accuracy, f1, roc
+    return accuracy, f1, precision, recall
 
 
 if __name__ == "__main__":
 
     df = pd.read_csv("./df_clear.csv")
 
-    X, Y = scale_frame(df)
+    X, Y, scaler = scale_frame(df)
 
     X_train, X_val, y_train, y_val = train_test_split(
-        X, Y,
+        X,
+        Y,
         test_size=0.3,
         random_state=42
     )
 
     params = {
         'alpha': [0.0001, 0.001, 0.01],
-        'l1_ratio': [0.05, 0.1, 0.2],
-        'penalty': ["l1", "l2", "elasticnet"],
-        'loss': ['log_loss', 'hinge', 'modified_huber'],
-        'fit_intercept': [False, True],
+        'l1_ratio': [0.0, 0.15, 0.5],
+        "penalty": ["l1", "l2", "elasticnet"],
+        "loss": ["hinge", "log_loss"],
+        "fit_intercept": [True, False]
     }
 
     mlflow.set_experiment("adult income classification")
 
     with mlflow.start_run():
 
-        clf_base = SGDClassifier(random_state=42)
+        model = SGDClassifier(random_state=42)
 
         clf = GridSearchCV(
-            clf_base,
+            model,
             params,
             cv=3,
             n_jobs=4
@@ -69,7 +72,7 @@ if __name__ == "__main__":
 
         y_pred = best.predict(X_val)
 
-        accuracy, f1, roc = eval_metrics(y_val, y_pred)
+        accuracy, f1, precision, recall = eval_metrics(y_val, y_pred)
 
         mlflow.log_param("alpha", best.alpha)
         mlflow.log_param("l1_ratio", best.l1_ratio)
@@ -78,21 +81,24 @@ if __name__ == "__main__":
         mlflow.log_param("fit_intercept", best.fit_intercept)
 
         mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("f1_score", f1)
-        mlflow.log_metric("roc_auc", roc)
+        mlflow.log_metric("f1", f1)
+        mlflow.log_metric("precision", precision)
+        mlflow.log_metric("recall", recall)
 
         predictions = best.predict(X_train)
+
         signature = infer_signature(X_train, predictions)
 
         mlflow.sklearn.log_model(best, "model", signature=signature)
 
-        with open("sgd_income.pkl", "wb") as file:
+        with open("adult_model.pkl", "wb") as file:
             joblib.dump(best, file)
 
     dfruns = mlflow.search_runs()
 
     path2model = (
-        dfruns.sort_values("metrics.accuracy", ascending=False)
+        dfruns
+        .sort_values("metrics.f1", ascending=False)
         .iloc[0]['artifact_uri']
         .replace("file://", "") + '/model'
     )
